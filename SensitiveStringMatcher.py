92472e16eb3ce5bccc52311ed9f66c2f5fb1838d
bbean <bbean@sandia.gov>
2024-03-01 17:05:35 -0700

    better regex and negative pattern matching

--- SensitiveStringMatcher.py
+++ SensitiveStringMatcher.py
@@ -20,6 +20,7 @@ class SensitiveStringMatcher():
     def __init__(self, name: str, *patterns: str):
         self.name = name
         self.patterns: list[re.Pattern | str] = []
+        self.neg_patterns: list[re.Pattern | str] = []
         self.log_type = lt.log.DEBUG
         self.log = lt.debug
         self.compare_to: str = None
@@ -27,6 +28,7 @@ class SensitiveStringMatcher():
 
         next_is_regex = False
         all_regex = False
+        remaining_negative_match = False
         for pattern in patterns:
             if pattern.startswith("**"):
                 directive = pattern[2:]
@@ -50,56 +52,95 @@ class SensitiveStringMatcher():
                     all_regex = True
                 elif directive == "case_sensitive":
                     self.case_sensitive = True
+                elif directive == "dont_match":
+                    remaining_negative_match = True
 
             else:
                 if next_is_regex or all_regex:
-                    pattern = re.compile(pattern.lower())
+                    pattern = re.compile(pattern)
+                else:
+                    pass
+
+                if not remaining_negative_match:
                     self.patterns.append(pattern)
                 else:
-                    self.patterns.append(pattern)
+                    self.neg_patterns.append(pattern)
+
                 next_is_regex = False
+
+        # case insensitive matching
+        if not self.case_sensitive:
+            for i, pattern in enumerate(self.patterns):
+                if isinstance(pattern, str):
+                    self.patterns[i] = pattern.lower()
+                else:
+                    p: re.Pattern = pattern
+                    self.patterns[i] = re.compile(p.pattern.lower())
+
+    def _search_pattern(self, ihaystack: str, pattern: re.Pattern | str) -> None | list[int]:
+        if isinstance(pattern, str):
+            # Check for occurances of string literals
+            if pattern in ihaystack:
+                start = ihaystack.index(pattern)
+                end = start + len(pattern)
+                return [start, end]
+
+        else:
+            # Check for instances of regex matches
+            re_match = pattern.search(ihaystack)
+            if re_match:
+                if len(re_match.groups()) >= 1:
+                    line_part = re_match[1]
+                    start = ihaystack.index(line_part)
+                    end = start + len(line_part)
+                else:
+                    start, end = re_match.span()[0], re_match.span()[1]
+                return [start, end]
+
+        return None
+
+    def _search_patterns(self, ihaystack: str, patterns: list[re.Pattern | str]) -> dict[re.Pattern | str, list[int]]:
+        ret: dict[re.Pattern | str, list[int]] = {}
+
+        for pattern in patterns:
+            span = self._search_pattern(ihaystack, pattern)
+            if span:
+                ret[pattern] = span
+
+        return ret
 
     def check_lines(self, lines: list[str]):
         matches: list[Match] = []
 
         for lineno, line in enumerate(lines):
-            for pattern in self.patterns:
-                start, end = -1, -1
+            iline = line if self.case_sensitive else line.lower()
 
-                if isinstance(pattern, str):
-                    # Check for occurances of string literals
-                    ipattern = pattern
-                    iline = line
-                    if not self.case_sensitive:
-                        ipattern = pattern.lower()
-                        iline = line.lower()
-                    if ipattern in iline:
-                        start = iline.index(ipattern)
-                        end = start + len(pattern)
+            # Check for matching patterns in this line
+            possible_matching = self._search_patterns(iline, self.patterns)
 
-                else:
-                    # Check for instances of regex matches
-                    re_match = pattern.search(line.lower())
-                    if re_match:
-                        if len(re_match.groups()) >= 1:
-                            line_part = re_match[1]
-                            start = line.index(line_part)
-                            end = start + len(line_part)
-                        else:
-                            start, end = re_match.span()[0], re_match.span()[1]
+            # Filter out negative matches in the matching patterns
+            matching: dict[re.Pattern | str, list[int]] = {}
+            for pattern in possible_matching:
+                span = possible_matching[pattern]
+                line_part = iline[span[0]:span[1]]
+                if len(self._search_patterns(line_part, self.neg_patterns)) == 0:
+                    matching[pattern] = span
 
-                # There was a match, record it
-                if start >= 0 and end >= 0:
-                    line_part = f"`{line[start:end]}`"
-                    if start > 0:
-                        line_part = line[max(start - 5, 0):start] + line_part
-                    if end < len(line):
-                        line_part = line_part + line[end:min(end + 5, len(line))]
+            # Register the matches
+            for pattern in matching:
+                span = matching[pattern]
 
-                    match = Match(lineno, start, end, line, line_part, self)
-                    self.set_match_msg(match, pattern)
-                    matches.append(match)
-                    self.log(match.msg)
+                start, end = span[0], span[1]
+                line_part = f"`{line[start:end]}`"
+                if start > 0:
+                    line_part = line[max(start - 5, 0):start] + line_part
+                if end < len(line):
+                    line_part = line_part + line[end:min(end + 5, len(line))]
+
+                match = Match(lineno, start, end, line, line_part, self)
+                self.set_match_msg(match, pattern)
+                matches.append(match)
+                self.log(match.msg)
 
         if self.compare_to:
             return []  # TODO
@@ -108,5 +149,5 @@ class SensitiveStringMatcher():
 
     def set_match_msg(self, match: Match, pattern: re.Pattern | str):
         log_msg = f"'{self.name}' string matched to pattern '{pattern}' on line {match.lineno} " + \
-            f"[{match.colno}:{match.colend}]: \"{match.line_part}\" (\"{match.line.strip()}\")"
+            f"[{match.colno}:{match.colend}]: \"{match.line_part.strip()}\" (\"{match.line.strip()}\")"
         match.msg = log_msg
